01c752b30a100a2a2f5a33e930c20cb114b1de3b
Vinicius <57239647+Vindemoto@users.noreply.github.com>
2026-08-24 21:24:25 -0300

    Update sistemaEscola.java

--- Aula_2/sistemaEscola.java
+++ Aula_2/sistemaEscola.java
@@ -4,8 +4,10 @@ public class Aula_2 {
 
     public static void main(String[] args) {
         cadAluno aluno = new cadAluno();
-        aluno.cadastrarAluno("Vinícius", "18/01/2006", 676939, "vinicius.moreira18@gmail.com", "11 39670-6767");
-        aluno.verificarCadastro();
+        aluno.cadastrarAluno("Vinícius Teixeira Moreira", "18/01/2006", 676939, "vinicius.moreira18@gmail.com", "11 39670-6767");
+        aluno.exibirDados();
+        aluno.excluirDados();
+        aluno.exibirDados();
     }
     
 }
