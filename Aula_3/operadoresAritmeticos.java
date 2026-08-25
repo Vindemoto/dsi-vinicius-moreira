d498dfa12afa1b1a09820728fe6ae28eebabb093
Vinicius <57239647+Vindemoto@users.noreply.github.com>
2026-08-24 21:50:16 -0300

    Add files via upload

---
+++ Aula_3/operadoresAritmeticos.java
@@ -0,0 +1,13 @@
+package aula_3;
+
+public class Aula_3 {
+
+    public static void main(String[] args) {
+        int A = 1, B = 8, C = 4, D = 5, E = 2;
+        double R;
+        R = A + B - C * D / E;
+        System.out.println("1 + 8 - 4 * 5 / 2 = " + R);
+        System.exit(0);
+    }
+    
+}
